b0788718b9c795adaa2afda2c6c48ec6f74dadc4
jacobtc13 <9855432@student.swin.edu.au>
2015-09-23 16:25:46 +1000

    bug fixes

--- src/GameResources.cs
+++ src/GameResources.cs
@@ -302,10 +302,3 @@ public static class GameResources
 		SwinGame.ProcessEvents();
 	}
 }
-
-//=======================================================
-//Service provided by Telerik (www.telerik.com)
-//Conversion powered by NRefactory.
-//Twitter: @telerik
-//Facebook: facebook.com/telerik
-//=======================================================
